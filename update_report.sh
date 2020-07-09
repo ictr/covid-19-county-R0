d61f718bb60939fb5ac705e404636cf27df71101
Bo Peng <ben.bog@gmail.com>
2020-07-09 01:04:42 -0500

    Use sos version

--- update_report.sh
+++ update_report.sh
@@ -25,18 +25,17 @@ echo $newsum > $sumfile
 echo "Update md5 file"
 
 # build docker image if has not existed
-if [[ "$(docker images -q covid19-r0 2> /dev/null)" == "" ]]; then
-   docker build . -t covid19-r0
+if [[ "$(docker images -q covid19-r0-sos 2> /dev/null)" == "" ]]; then
+   docker build . -t covid19-r0-sos
 fi
 
 # process data
-docker run --rm -i -v $(pwd):/covid-19-county-R0 covid19-r0 sh -c 'cd /covid-19-county-R0/TSHS_CaseCountData; Rscript code.r' 
-docker run --rm -i -v $(pwd):/covid-19-county-R0 covid19-r0 sh -c 'cd /covid-19-county-R0/; papermill "Realtime R0.ipynb" Realtime_updated.ipynb'
+docker run --rm -i -v $(pwd):/covid-19-county-R0 covid19-r0-sos sh -c 'cd /covid-19-county-R0/TSHS_CaseCountData; Rscript code.r'
+docker run --rm -i -v $(pwd):/covid-19-county-R0 covid19-r0-sos sh -c 'cd /covid-19-county-R0/; papermill --engine sos "Realtime R0_sos.ipynb" Realtime_updated.ipynb'
 
 # update title with the current date and convert to HTML file
 sed -i -E "s/in Real-Time \(Until .+\)/in Real-Time \(Until $(date +"%b %d")\)/" Realtime_updated.ipynb
-docker run --rm -i -v $(pwd):/covid-19-county-R0 covid19-r0 sh -c 'cd /covid-19-county-R0/; jupyter nbconvert --to html Realtime_updated.ipynb'
+docker run --rm -i -v $(pwd):/covid-19-county-R0 covid19-r0-sos sh -c 'cd /covid-19-county-R0/; sos convert Realtime_updated.ipynb Realtime_updated.html --template sos-report-v2'
 
 # move updated HTML file to webserver
 mv Realtime_updated.html "/var/www/web/sites/default/files/r0.html"
-
