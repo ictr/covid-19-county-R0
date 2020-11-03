2a2cf3de3142675aad4eb61f69a33caf6c25ba8e
YiPeng-Gao <yipeng.gao@bcm.edu>
2020-11-03 11:33:45 -0800

    Add existing files

--- TSHS_CaseCountData/code.r
+++ TSHS_CaseCountData/code.r
@@ -13,14 +13,14 @@ my_data = my_data[, -2]
 colnames(my_data)[1] = "county"
 my_data = as.data.frame(my_data)
 
-# calculate new daily cases after Nov.1st
-nov.01 = which(colnames(my_data) == "20201101")
+# calculate new daily cases after specified days
+selected_date = which(colnames(my_data) == "20201102")
 ## daily new cases dataframe
-if((ncol(my_data)-nov.01) >= 1){
-  daily_new_cases = data.frame(matrix(nrow = 254, ncol = (ncol(my_data) - nov.01)))
-  colnames(daily_new_cases) = colnames(my_data)[(nov.01+1):ncol(my_data)]
-  for(i in 1:(ncol(my_data)-nov.01)){
-    daily_new_cases[,i] = my_data[,nov.01+i] - my_data[,nov.01+i-1]
+if((ncol(my_data)-selected_date) >= 1){
+  daily_new_cases = data.frame(matrix(nrow = 254, ncol = (ncol(my_data) - selected_date)))
+  colnames(daily_new_cases) = colnames(my_data)[(selected_date+1):ncol(my_data)]
+  for(i in 1:(ncol(my_data)-selected_date)){
+    daily_new_cases[,i] = my_data[,selected_date+i] - my_data[,selected_date+i-1]
   }
 }
 
@@ -28,7 +28,7 @@ if((ncol(my_data)-nov.01) >= 1){
 new_data = read_excel("DSHS New County Data.xlsx", col_names=T, n_max=9)
 new_data_date = seq(20200901,20200930,1)
 new_data_date_oct = seq(20201001,20201031,1)
-new_data_date_nov = seq(20201101,20201101,1)
+new_data_date_nov = seq(20201101,20201102,1)
 new_data_date = append(new_data_date, new_data_date_oct)
 new_data_date = append(new_data_date, new_data_date_nov)
 colnames(new_data) = c("county", new_data_date)
@@ -53,10 +53,10 @@ for(i in 1:length(counties)){
   my_data[which(my_data$county == county),] = my_data_county
 }
 
-# update data after Nov.1
-if((ncol(my_data)-nov.01) >= 1){
-  for(i in 1:(ncol(my_data)-nov.01)){
-    my_data[,nov.01+i] = my_data[,nov.01+i-1] + daily_new_cases[,i]
+# update data after Nov.2
+if((ncol(my_data)-selected_date) >= 1){
+  for(i in 1:(ncol(my_data)-selected_date)){
+    my_data[,selected_date+i] = my_data[,selected_date+i-1] + daily_new_cases[,i]
   }
 }
 
